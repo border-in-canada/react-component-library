5234b335db7d188d2ddfbf798e17588225ce4310
border-in-canada <mathesd84@gmail.com>
2023-09-25 22:00:23 -0700

    Remove node modules from version control

---
+++ dist/esm/types/components/Input/Input.d.ts
@@ -0,0 +1,6 @@
+import React from "react";
+export interface InputProps {
+    label: string;
+}
+declare const Input: (props: InputProps) => React.JSX.Element;
+export default Input;
---
+++ dist/index.d.ts
@@ -0,0 +1,8 @@
+import React from 'react';
+
+interface InputProps {
+    label: string;
+}
+declare const Input: (props: InputProps) => React.JSX.Element;
+
+export { Input };
