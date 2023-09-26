8fe79b3c58b71226d1e04ed6e9296e541de0bc3f
border-in-canada <mathesd84@gmail.com>
2023-09-26 14:52:27 -0700

    Add control to input component

--- src/components/Input/Input.stories.tsx
+++ src/components/Input/Input.stories.tsx
@@ -3,12 +3,15 @@ import type { Meta, StoryObj } from '@storybook/react';
 import Input from "./Input";
 
 const meta: Meta<typeof Input> = {
-    component: Input,
+    component: Input
 };
 
 export default meta;
 type Story = StoryObj<typeof Input>;
 
 export const Primary: Story = {
-    render: () => <Input label="Input" />,
+    args: {
+        label: 'Input',
+        name: 'input'
+    }
 };
